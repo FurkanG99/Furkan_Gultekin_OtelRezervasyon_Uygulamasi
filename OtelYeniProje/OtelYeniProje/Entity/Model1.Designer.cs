7eabaae499be18880f09b7b9324064011ed248fb
FurkanG99 <110032519+FurkanG99@users.noreply.github.com>
2024-04-15 19:59:26 +0300

    silip yüklele düzenleme

--- OtelYeniProje/OtelYeniProje/Entity/Model1.Designer.cs
+++ OtelYeniProje/OtelYeniProje/Entity/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\furka\OneDrive\Belgeler\GitHub\Furkan_Gultekin_OtelRezervasyon_Uygulamasi\OtelYeniProje\OtelYeniProje\Entity\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\14may\source\repos\OtelYeniProje\OtelYeniProje\Entity\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
